f888751c176f6897ad066bd5bd94af2365ebb5d9
EstefaniaSalamanca <estefaniasalamanca@hotmail.com>
2023-04-15 16:03:03 +0200

    Recycleview superheros

---
+++ app/src/main/java/com/example/recyclerview/adapter/SuperHeroViewHolder.kt
@@ -0,0 +1,28 @@
+package com.example.recyclerview.adapter
+
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.recyclerview.SuperHero
+import com.example.recyclerview.databinding.ItemSuperheroBinding
+
+class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val binding = ItemSuperheroBinding.bind(view)
+
+
+    fun render(superHeroModel: SuperHero, onClickListener: (SuperHero) -> Unit) {
+        binding.tvSuperHeroName.text = superHeroModel.superhero
+        binding.tvRealName.text = superHeroModel.realName
+        binding.tvPublisher.text = superHeroModel.publisher
+        Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
+
+        itemView.setOnClickListener {
+            onClickListener(superHeroModel)
+
+        }
+
+    }
+}
